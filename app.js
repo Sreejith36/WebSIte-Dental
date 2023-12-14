acb01342a99b8dec2e88cc1073db5e336c917857
Sreejith36 <sreejith8230@gmail.com>
2023-12-14 14:29:24 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
